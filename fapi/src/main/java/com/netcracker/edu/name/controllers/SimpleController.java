237999c43c3b7897d791d4d34e9d14ebc404aecb
Arina Pinchuk <arina.pinchuk1@gmail.com>
2019-03-18 19:29:57 +0300

    first step

---
+++ fapi/src/main/java/com/netcracker/edu/name/controllers/SimpleController.java
@@ -0,0 +1,38 @@
+package com.netcracker.edu.name.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleController {
+
+    @RequestMapping(value = "/get-simple-object")
+    SimpleObject getSimpleString() {
+        SimpleObject simpleObject= new SimpleObject();
+        simpleObject.setId(12345);
+        simpleObject.setName("Arina");
+        return simpleObject;
+    }
+
+}
+class SimpleObject{
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    int id;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    String name;
+}
